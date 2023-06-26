8db51f22acd647538535ce26fd0f94c0a0fff4ab
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-06-26 05:49:29 +0000

    refactor password field

--- app/Login/page.jsx
+++ app/Login/page.jsx
@@ -24,16 +24,11 @@ const Login = () => {
               />
             </div>
             <div className="mb-2">
-              <label htmlFor="password" className="block text-sm font-medium text-gray-900 dark:text-white">
-                Password
-              </label>
-              <input
+            <TextInput 
                 type="password"
                 name="password"
-                id="password"
-                placeholder="••••••••"
-                required
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text"
+                label="Password"
+                placeholder="Password"             
               />
             </div>
              <div  className='mt-4'>
