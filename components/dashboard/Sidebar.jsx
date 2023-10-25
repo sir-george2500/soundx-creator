3cd331a57251d2e42df49bf5fff5a90a8c2e3224
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-10-25 03:41:23 +0000

    added current stickly navigation

--- components/dashboard/Sidebar.jsx
+++ components/dashboard/Sidebar.jsx
@@ -1,35 +1,36 @@
 import { useEffect, useState } from 'react';
-import { mdiChartBox, mdiCurrencyUsd, mdiMusic} from '@mdi/js';
+import { mdiChartBox, mdiCurrencyUsd, mdiMusic } from '@mdi/js';
 import Icon from '@mdi/react';
 import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
 
 const Sidebar = () => {
+  const params = useSearchParams();
+  const view = params.get('view');
+
   return (
-
-<div className="h-70 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1 mt-1">
-  <div className="bg-neutral-950 h-full p-6">
-    <h1 className="text-3xl font-extrabold inline-block text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-red-500 bg-yellow-600 mb-3 p-2">Menu</h1>
-    <ul className="text-white text-lg list-none space-y-4 ">
-      <Link href="/board?view=overview">
-        <li className="transition duration-300 flex items-center p-2 hover:bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 hover:bg-clip-text hover:text-transparent">
-          <Icon path={mdiChartBox} size={1} className="mr-2 hover:fill-current" /> Overview
-        </li>
-      </Link>
-      <Link href="/board?view=music">
-        <li className="transition duration-300 flex  items-center p-2 hover:bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 hover:bg-clip-text hover:text-transparent">
-          <Icon path={mdiMusic} size={1} className="mr-2 hover:fill-current" /> My Music
-        </li>
-      </Link>
-      <Link href="/board?view=earning">
-        <li className="transition duration-300 flex items-center p-2 hover:bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 hover:bg-clip-text hover:text-transparent">
-          <Icon path={mdiCurrencyUsd} size={1} className="mr-2 hover:fill-current" /> Earning
-        </li>
-      </Link>
-    </ul>
-  </div>
-</div>
-
-
+    <div className="h-70 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1 mt-1">
+      <div className="bg-neutral-950 h-full p-6">
+        <h1 className="text-3xl font-extrabold inline-block text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-red-500 bg-yellow-600 mb-3 p-2">Menu</h1>
+        <ul className="text-white text-lg list-none space-y-4 ">
+          <Link href="/board?view=overview">
+            <li className={`transition duration-300 flex items-center p-2 ${view === 'overview' ? 'bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-transparent' : 'hover:bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 hover:bg-clip-text hover:text-transparent'}`}>
+              <Icon path={mdiChartBox} size={1} className="mr-2 hover:fill-current" /> Overview
+            </li>
+          </Link>
+          <Link href="/board?view=music">
+            <li className={`transition duration-300 flex items-center p-2 ${view === 'music' ? 'bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-transparent' : 'hover:bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 hover:bg-clip-text hover:text-transparent'}`}>
+              <Icon path={mdiMusic} size={1} className="mr-2 hover:fill-current" /> My Music
+            </li>
+          </Link>
+          <Link href="/board?view=earning">
+            <li className={`transition duration-300 flex items-center p-2 ${view === 'earning' ? 'bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-transparent' : 'hover:bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 hover:bg-clip-text hover:text-transparent'}`}>
+              <Icon path={mdiCurrencyUsd} size={1} className="mr-2 hover:fill-current" /> Earning
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </div>
   );
 };
 
