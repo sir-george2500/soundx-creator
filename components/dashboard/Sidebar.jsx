97612ce6d474fe92d2cfdf8f2e432641d82dab2d
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-10-25 03:30:33 +0000

    added gradient to text

--- components/dashboard/Sidebar.jsx
+++ components/dashboard/Sidebar.jsx
@@ -5,28 +5,31 @@ import Link from 'next/link';
 
 const Sidebar = () => {
   return (
+
 <div className="h-70 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1 mt-1">
-    <div className="bg-neutral-950 h-full p-6">
-      <h1 className="text-3xl font-extrabold text-white mb-6">Menu</h1>
-      <ul className="text-white text-lg list-none space-y-4">
-        <Link href="/board?view=overview">
-          <li className="hover:text-amber-500 transition duration-300 flex items-center">
-            <Icon path={mdiChartBox} size={1} className="mr-2" /> Overview
-          </li>
-        </Link>
-        <Link href="/board?view=music">
-        <li className="hover:text-yellow-300 transition duration-300 flex  items-center ">
-          <Icon path={mdiMusic} size={1} className="mr-2" /> Music
+  <div className="bg-neutral-950 h-full p-6">
+    <h1 className="text-3xl font-extrabold inline-block text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-red-500 bg-yellow-600 mb-3 p-2">Menu</h1>
+    <ul className="text-white text-lg list-none space-y-4 ">
+      <Link href="/board?view=overview">
+        <li className="transition duration-300 flex items-center p-2 hover:bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 hover:bg-clip-text hover:text-transparent">
+          <Icon path={mdiChartBox} size={1} className="mr-2 hover:fill-current" /> Overview
         </li>
-        </Link>
-        <Link href="/board?view=earning">
-        <li className="hover:text-yellow-300 transition duration-300 flex items-center">
-          <Icon path={mdiCurrencyUsd} size={1} className="mr-2" /> Earning
+      </Link>
+      <Link href="/board?view=music">
+        <li className="transition duration-300 flex  items-center p-2 hover:bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 hover:bg-clip-text hover:text-transparent">
+          <Icon path={mdiMusic} size={1} className="mr-2 hover:fill-current" /> My Music
         </li>
-        </Link>
-      </ul>
-    </div>
+      </Link>
+      <Link href="/board?view=earning">
+        <li className="transition duration-300 flex items-center p-2 hover:bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 hover:bg-clip-text hover:text-transparent">
+          <Icon path={mdiCurrencyUsd} size={1} className="mr-2 hover:fill-current" /> Earning
+        </li>
+      </Link>
+    </ul>
+  </div>
 </div>
+
+
   );
 };
 
